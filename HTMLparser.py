c97a63420d95cf875a88dac8fe9a5cd59f51de0c
ben Cookson <98530526+MITwannabe101@users.noreply.github.com>
2022-05-05 14:07:14 +0100

    Updated HTMLparser to recieve commands encrypted by server

--- HTMLparser.py
+++ HTMLparser.py
@@ -10,12 +10,33 @@ class HTMLclient:
         
     def recievecommands(self, commandlink=''):
         """GET request to web page to get commands"""
-        if commandlink=='': commandlink=self.commandlink
+        if commandlink=='': commandlink=self.commandlink #makes sure there is link
+        
+        #gets html
         with request.urlopen(commandlink) as response:
-            rawcommands = response.read().decode()
-        commands = json.loads(rawcommands)
+            html = response.read().decode() #recieved commands
+
+        """hidden json:
+            1) add random chareceter after every charecter in json- e.g {"a":"b"} -> A{B"Fa("G:L"XbK"A}
+            2) encrypt into base64
+            3) put in html""""
+            
+        #extracts hidden json
+        Hjson_start, Hjson_end = 0, 0 #hidden jason start and end
+        while html[Hjson_start] != '{': Hjson_start += 1
+        while html[Hjson_end] != '}': Hjson_end -= 1
+        Hjson = html[Hjson_start:Hjson_end+1]
+        
+        #gets json from hidden json
+        jsonStr = ''
+        for i in range(0, len(Hjson), 2):
+                jsonStr += str(Hjson[i])
+        
+        #converts jsonStr to dictionary
+        commands = json.loads(jsonStr)
+        
         return commands
 
 if __name__ == '__main__':
-    x = HTMLclient('https://api.wheretheiss.at/v1/satellites/25544')
-    print(x.recievecommands())
+    #x = HTMLclient('https://api.wheretheiss.at/v1/satellites/25544')
+    #print(x.recievecommands())
