ff6dbbbce59381a197ed7f3e1b497a2449827b88
ben Cookson <98530526+MITwannabe101@users.noreply.github.com>
2022-05-06 15:24:54 +0100

    Update commandEncryptor.py

--- httpTrojan/commandEncryptor.py
+++ httpTrojan/commandEncryptor.py
@@ -1,11 +1,16 @@
 import random
 
-def encrypt(json)
-  while True:
+def encrypt(json):
       string = ''
+      #add random characters
       for i in range(0, len(json)):
           string += json[i]
-          string += chr(random.randint(32, 90))
-      enc = str(string.encode())[2:-1]
-      html = '<!doctype="html"><h3><hr>'+enc+'</h3></doctype>'
-      print(html)
+          string += chr(random.randint(32, 60))
+      print(string)
+      strUTF8 = str(string.encode("utf-8"))[2:1]
+      print(strUTF8)
+      strB2 = ''.join(format(ord(i), '08b') for i in strUTF8) 
+
+      html = '<!doctype html><body><h3>'+strB2+'</h3></body></doctype>'
+      return html
+
