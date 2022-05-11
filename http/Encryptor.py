c6451b614103fc403803966df2b83495b7c8a46b
ben Cookson <98530526+MITwannabe101@users.noreply.github.com>
2022-05-11 10:38:55 +0100

    Update Encryptor.py

--- http/Encryptor.py
+++ http/Encryptor.py
@@ -12,24 +12,25 @@ def encrypt(plaintext: str) -> str:
     binarytext = ''.join(format(ord(i), '08b') for i in randomtext)
     binarytext = binarytext
     #convert binary to base64
-    base64text = base64.b64encode(binarytext.encode('utf-8'))
+    base64text = str(base64.b64encode(binarytext.encode('utf-8')))[2:-1]
     #adds random charecters to base64
     randombase64text = ''
     for i in range(len(str(base64text))):
         randombase64text  += str(base64text)[i]
         randombase64text  += chr(random.randint(33, 126))
-    html = f'<!doctype html><body>{randombase64text}</body></doctype>'
-    return html
+    html = ('<!doctype html><body>^%s^</body></doctype>' % randombase64text)
+    return binarytext, base64text #html
+
 
 def decrypt(html : str) -> str:
     #remove html
-    base64text = html[21:-17]
+    base64text = html[html.find('^'):html[::-1].find('^')]
     print(base64text, '\n\n')
     #removes random charecters
-    base64text_norandom = base64text[2:-1:2].encode('utf-8')[2:-1]
+    base64text_norandom = base64text[::2]
     print(base64text_norandom, '\n\n')
     #decode from base64
-    binarytext = base64.b64decode(base64text_norandom).decode()
+    binarytext = base64.b64decode(base64text_norandom.encode('latin-1'))
     ###print('binary:   ', binarytext)
     #converts from binary to text
     randomtext = ''.join(chr(int(binarytext[i*8:i*8+8],2)) for i in range(len(binarytext)//8))
