08d9c5a159a096d2793de433943f740c51d6e329
ben Cookson <98530526+MITwannabe101@users.noreply.github.com>
2022-06-15 12:50:59 +0100

    Update IPbruter.py

--- http/client/inbuilt/IPbruter.py
+++ http/client/inbuilt/IPbruter.py
@@ -1,6 +1,7 @@
 import subprocess, re
 import socket
 import threading
+from time import sleep
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ports = [443,19005,22,17775,80,445,15182,5060,5341,53,4978,1723,4718,8080,4207,21,2777,25,2350,4567,2328,3389,2200,8081,1856,8000,1841,110,1630,23,1418,10000,1396,81,1228,8082,1228,5000,1208,445,1072,143,1024,111,957,7547,893,
          135,873,587,846,139,818,28960,813,29900,804,18067,803,27374,801,1024,747,30005,711,7676,705,389,691,1026,678,4444,661,1025,628,1027,604,20,588,1050,555,1028,539,5678,533,1029,526,1863,507,8594,490,3783,482,1002,473,4664,472,37]
@@ -10,6 +11,11 @@ class IPExtractionUnit:
 
     def __call__(self):
         raw = ''
+        try:
+            with open('ips.txt', 'r') as file:
+                raw += file.read()
+        except:
+            pass
         for command in self.commands:
             try: raw += subprocess.check_output(command).decode()
             except: pass
@@ -33,36 +39,45 @@ class IPExtractionUnit:
                     if len(ip.split(':')) > 1 and ip.split(':')[-1]: ips.append((ip.split(':')[0], int(ip.split(':')[1])))
                     else: ips.append((ip.split(':')[0], None))
             i+= 1
-        return ips
+        return set(ips)
 
 
-class IpTester:
-    def __init__(addrs, ports):
+class IPTestingUnit:
+    def __init__(self, addrs, ports):
         self.addrs = addrs
         self.ports = ports
-        self.threads = []
+        self.open = []
 
     def run(self):
         for addr in self.addrs:
             if addr[1] != None:
-                self.threads.append(therading.Thread(target=self.test, args=(addr[0], addr[1])))
+                threading.Thread(target=self.test, args=(addr[0], addr[1])).start()
             else:
                 for port in self.ports:
-                    self.threads.append(therading.Thread(target=self.test, args=(addr[0], port)))
+                    threading.Thread(target=self.test, args=(addr[0], port)).start()
+        self.display()
+        
+    def display(self):
+        for found in self.open:
+            if type(found[2]) == None:
+                print('[*] {}:{} service- unable to identify'.format(found[0], found[1]))
+            else:
+                if type(found[2]) == None: #not empty
+                    print('[*] {}:{} service- {}\\{}'.format(found[0], found[1], found[2][0], found[2][1]))
+                else:
+                    print('[*] {}:{} service- {}'.format(found[0], found[1], found[2][0]))
+
+    def test(self, addr, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((addr,port))
+        sock.close()
+        if result == 0:
+                try: hostname = socket.gethostbyaddr(addr)
+                except: hostname = None
+                self.open.append((addr, port, hostname))
+
+
                 
-
-addr = '10.4.207.247'
-port = 442
-result = sock.connect_ex((addr,port))
-sock.close()
-
-if result == 0:
-    machine_hostname = socket.gethostbyaddr(addr)[0]
-    try: service = socket.getservbyport(port, 'tcp')
-    except OSError: service = 'N/A'
-    print('open port detected: ' + str(addr) + '\t-- Port: ' + str(port))
-    print(machine_hostname)
-else:
-    print('unablie to connect')
-
+IPtester = IPTestingUnit(IPExtractionUnit('arp -a', 'netstat', 'systeminfo')(), ports)
+IPtester.run()
 
